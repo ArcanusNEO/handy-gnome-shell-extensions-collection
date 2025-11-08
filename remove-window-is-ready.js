8cd1057484111e74d936e39e58edb45eb7728694
ArcanusNEO <admin@transcliff.eu.org>
2025-11-08 20:51:20 +0800

    Synchronize upstream code

--- remove-window-is-ready.js
+++ remove-window-is-ready.js
@@ -4,22 +4,23 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js'
 import GObject from 'gi://GObject'
 
-const blockSignal = (id) => {
-  const signalId = GObject.signal_handler_find(global.display, { signalId: id })
-  GObject.signal_handler_block(global.display, signalId)
+const getSignalHandlerId = (signalId) => {
+  return GObject.signal_handler_find(global.display, { signalId })
 }
-const unblockSignal = (id) => {
-  const signalId = GObject.signal_handler_find(global.display, { signalId: id })
-  GObject.signal_handler_unblock(global.display, signalId)
+const blockSignal = (signalId) => {
+  const signalHandlerId = getSignalHandlerId(signalId)
+  GObject.signal_handler_block(global.display, signalHandlerId)
+}
+
+const unblockSignal = (signalId) => {
+  const signalHandlerId = getSignalHandlerId(signalId);
+  GObject.signal_handler_unblock(global.display, signalHandlerId)
 }
 const enable = () => {
   blockSignal('window-demands-attention')
   blockSignal('window-marked-urgent')
 }
 const disable = () => {
-  if (Main.sessionMode.isLocked) {
-    return
-  }
   unblockSignal('window-demands-attention')
   unblockSignal('window-marked-urgent')
 }
